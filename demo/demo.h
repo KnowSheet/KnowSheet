69c37854e4e0dc4a9b30e62c344e10244dd66897
John Babak <babak.john@gmail.com>
2015-02-15 08:04:46 +0300

    demo: Added MIME type detection and stub layout, meta and data

--- demo/demo.h
+++ demo/demo.h
@@ -27,6 +27,9 @@ SOFTWARE.
 #ifndef DEMO_DEMO_H
 #define DEMO_DEMO_H
 
+#include <algorithm>
+#include <string>
+
 #include "../Bricks/dflags/dflags.h"
 
 #include "uptime.h"
@@ -42,32 +45,133 @@ using bricks::net::api::Request;
 using bricks::net::HTTPResponseCode;
 using namespace bricks::gnuplot;
 
-struct DemoServer {
+class DemoServer {
+ public:
   DemoServer(int port = FLAGS_demo_port) : port_(port) {
+    printf("Preparing to listen on port %d...\n", port_);
     HTTP(port).Register("/ok", [](Request r) { r.connection.SendHTTPResponse("OK\n"); });
     HTTP(port).Register("/uptime", UptimeTracker());
-    HTTP(port).Register("/", State::ClassBoundaries);
-    FileSystem::ScanDir("static", [&port](const std::string& fn) {
+    HTTP(port).Register("/yingyang.svg", State::ClassBoundaries);
+    HTTP(port).Register("/config.json", [](Request r) {
+      // TODO(sompylasar): Use cerealize.
+      r.connection.SendHTTPResponse(
+          "{}\n",
+          HTTPResponseCode::OK, "application/json");
+    });
+    HTTP(port).Register("/layout/data", [](Request r) {
+      // TODO(sompylasar): Use cerealize.
+      // TODO(sompylasar): Use chunked response.
+      r.connection.SendHTTPResponse(
+          "{\"x\":0.0,\"y\":1.0}\n",
+          HTTPResponseCode::OK, "application/json");
+    });
+    HTTP(port).Register("/layout/meta", [](Request r) {
+      // TODO(sompylasar): Use cerealize.
+      r.connection.SendHTTPResponse(
+        "\
+{\n\
+  \"meta\":{\n\
+    \"data_url\":\"/data\",\n\
+    \"visualizer_name\":\"value-visualizer\",\n\
+    \"visualizer_options\":{\n\
+      \"header_text\":\"Demo Value\",\n\
+      \"higher_is_better\":true\n\
+    }\n\
+  }\n\
+}\n",
+          HTTPResponseCode::OK,
+          "application/json");
+    });
+    HTTP(port).Register("/layout", [](Request r) {
+      // TODO(sompylasar): Use cerealize.
+      r.connection.SendHTTPResponse(
+          "\
+{\n\
+  \"layout\":{\n\
+    \"col\":[\n\
+      {\n\
+        \"cell\":{\n\
+          \"meta_url\":\"/meta\"\n\
+        }\n\
+      }\n\
+    ]\n\
+  }\n\
+}\n",
+          HTTPResponseCode::OK, "application/json");
+    });
+    // The "./static/" directory should be a symlink to "Web/build" or "Web/build-dev".
+    FileSystem::ScanDir("./static/", [&port](const std::string& filename) {
+      std::string filepath = "./static/" + filename;
+      std::string fileurl = "/static/" + filename;
+      // The default route points to the frontend entry point file.
+      if (filename == "index.html") {
+        fileurl.assign("/");
+      }
       // Hack with string ownership.
       // TODO(dkorolev): Make this cleaner.
-      std::string* ps = new std::string();
-      *ps = FileSystem::ReadFileAsString("static/" + fn);
-      HTTP(port).Register("/static/" + fn, [ps](Request r) {
-        r.connection.SendHTTPResponse(*ps, HTTPResponseCode::OK, "text/plain");
+      std::string* contentType = new std::string(DemoServer::GetFileMimeType(filename));
+      std::string* content = new std::string();
+      *content = FileSystem::ReadFileAsString(filepath);
+      HTTP(port).Register(fileurl, [content, contentType](Request r) {
+        r.connection.SendHTTPResponse(*content, HTTPResponseCode::OK, *contentType);
       });
     });
   }
 
   void Join() {
-    using bricks::net::api::HTTP;
-
     printf("Listening on port %d\n", port_);
     HTTP(port_).Join();
     printf("Done.\n");
   }
 
+ private:
   State state_;
   const int port_;
+
+  /**
+   * Gets the file extension from the file name or path.
+   */
+  static const std::string GetFileExtension(const std::string& filename) {
+    size_t fileextIndex = filename.find_last_of("/\\.");
+
+    if (fileextIndex == std::string::npos || filename[fileextIndex] != '.') {
+      return "";
+    }
+
+    // Add 1 to skip the dot.
+    std::string fileext = filename.substr(fileextIndex + 1);
+
+    return fileext;
+  }
+
+  /**
+   * Detects MIME type by the file extension.
+   */
+  static const std::string GetFileMimeType(const std::string& filename) {
+    std::string fileext = DemoServer::GetFileExtension(filename);
+
+    std::transform(fileext.begin(), fileext.end(), fileext.begin(), ::tolower);
+
+    std::map<std::string, std::string> mimeTypesByExtension = {
+      {"js", "application/javascript"},
+      {"json", "application/json"},
+      {"css", "text/css"},
+      {"html", "text/html"},
+      {"htm", "text/html"},
+      {"txt", "text/plain"},
+      {"png", "image/png"},
+      {"jpg", "image/jpeg"},
+      {"jpeg", "image/jpeg"},
+      {"gif", "image/gif"},
+      {"svg", "image/svg"}
+    };
+
+    if (mimeTypesByExtension.count(fileext)) {
+      return mimeTypesByExtension.at(fileext);
+    }
+
+    return "text/plain";
+  }
 };
 
 }  // namespace demo
